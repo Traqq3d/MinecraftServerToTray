ba52a654216528332a1a57de043418d42ce4cacd
jdunne <jdunne525@hotmail.com>
2020-04-23 21:40:01 -0500

    X0.m.7 Improved batch file to automatically start MinecraftServerToTray.exe
    _Branch:master

--- Start_minecraft_server.bat
+++ Start_minecraft_server.bat
@@ -1,2 +1,4 @@
+TASKKILL /F /IM "MinecraftServerToTray.exe" > nul 2>&1
+start /B MinecraftServerToTray.exe
 TITLE Minecraft server console window
 "C:\Program Files (x86)\Java\jre1.8.0_241\bin\java.exe" -Xmx1024M -Xms1024M -jar server.jar nogui
